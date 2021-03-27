414f3bbde871a3666546e7fda2a5056cae39f175
Lucas Walter <wsacul@gmail.com>
2021-03-27 08:11:19 -0700

    Use util delay instead of arduino delays

--- src/test.ino
+++ src/test.ino
@@ -2,15 +2,41 @@
 Copyright (C) 2021 Lucas Walter
  */
 
+// #include <avr/sleep.h>
+#include <util/delay.h>
+
+unsigned long start_time = 0;
+
 void setup() {
   Serial.begin(9600);
+  Serial.println("init test");
+  start_time = millis();
 }
 
+int count = 0;
+int count2 = 0;
+
 void loop() {
-  // read the input on analog pin 0:
-  // int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println("TEST1");
-  Serial.println("FOO");
-  delay(1);        // delay in between reads for stability
+  // int value = analogRead(A0);
+  // Serial.println(count);
+  count += 1;
+  if (count > 100) {
+    // TODO(lucasw) millis doesn't work in qemu?
+    unsigned long cur_t = millis();
+    unsigned long elapsed_t = cur_t - start_time;
+    // Serial.println(cur_t);
+    // Serial.println(elapsed_t);
+    Serial.println("loop");
+    Serial.println(count2);
+    count2 += 1;
+    count = 0;
+    /*
+    cli();
+    sleep_enable();
+    sleep_cpu();
+    */
+  }
+  // delayMicroseconds(10000);
+  _delay_ms(1000);
+  // delay(1);        // delay in between reads for stability
 }
